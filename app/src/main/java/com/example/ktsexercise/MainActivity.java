6dd7e37f777e88050a9707fa2d37d3f1b116d704
achavezz <chavezalex986@gmail.com>
2022-11-02 14:33:34 -0500

    Initialization of item models created in MainActivity class

--- app/src/main/java/com/example/ktsexercise/MainActivity.java
+++ app/src/main/java/com/example/ktsexercise/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<ItemModel> itemModels = new ArrayList<>();
+    int[] itemImages = {R.drawable.apple, R.drawable.avocado, R.drawable.banana, R.drawable.chicken, R.drawable.cocacola,
+        R.drawable.hotcheetos, R.drawable.icecream, R.drawable.mango, R.drawable.orange, R.drawable.oreocookies,
+        R.drawable.papertowels, R.drawable.pepsi, R.drawable.pineapple, R.drawable.strawberries, R.drawable.toilerpaper, R.drawable.waterbottle};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpItemModels();
+
+    }
+
+    private void setUpItemModels(){
+        String[] itemNames = getResources().getStringArray(R.array.grocery_names_txt);
+        int[] itemIDs = getResources().getIntArray(R.array.grocery_id_txt);
+        int[] itemCounts = getResources().getIntArray(R.array.grocery_item_count_txt);
+
+        for (int i = 0; i < itemNames.length; i++){
+            itemModels.add(new ItemModel(itemNames[i], itemIDs[i], itemCounts[i], itemImages[i]));
+        }
     }
 }
