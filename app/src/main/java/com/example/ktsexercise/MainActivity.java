a6934e06f44e1e81a51ab9f706d64dd12f12c358
achavezz <chavezalex986@gmail.com>
2022-11-03 18:30:34 -0500

    Fixed bug where user list was overriden when activity swapping

--- app/src/main/java/com/example/ktsexercise/MainActivity.java
+++ app/src/main/java/com/example/ktsexercise/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements RecylerViewInterf
     }
 
     private void setUpItemModels(){
+        userItemModels = PrefConfig.readListFromPref(this);
         String[] itemNames = getResources().getStringArray(R.array.grocery_names_txt);
         int[] itemIDs = getResources().getIntArray(R.array.grocery_id_txt);
         int[] itemCounts = getResources().getIntArray(R.array.grocery_item_count_txt);
